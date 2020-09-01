5c2eb3851e197fce914e1e29cc3a3ecf4626ebb9
Arisien <67979860+Arisien@users.noreply.github.com>
2020-09-01 16:20:10 +0200

    arrow key based game, colors added

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,28 @@ class Field {
 int height, width;
 Field field[24][24];
 
-//Prints helpful coordinates
-void printh (int i) {
-  rlutil::setColor(3);
-  cout << i%10 << " ";
-  rlutil::resetColor();
-}
-
 //Prints normal points
-void printc (int i, int j) {
+void printc (int i, int j, bool cursor) {
+  if (cursor) {
+    rlutil::setBackgroundColor(15);
+    rlutil::setColor(0);
+  }
+  else if (!field[i][j].visible) rlutil::setColor(7);
+  else if (field[i][j].value == 1) rlutil::setColor(1);
+  else if (field[i][j].value == 2) rlutil::setColor(2);
+  else if (field[i][j].value == 3) rlutil::setColor(12);
+  else if (field[i][j].value == 4) rlutil::setColor(13);
+  else if (field[i][j].value == 5) rlutil::setColor(4);
+  else if (field[i][j].value == 6) rlutil::setColor(11);
+  else if (field[i][j].value == 7) rlutil::setColor(14);
+  else if (field[i][j].value == 8) rlutil::setColor(8);
+
   if(field[i][j].flagged) cout << "F";
-  else if (!field[i][j].visible) cout << "*";
+  else if (!field[i][j].visible) cout << "#";
   else if (field[i][j].value == 9) cout << "B";
+  else if (field[i][j].value == 0) cout << " ";
   else cout << field[i][j].value;
+  rlutil::resetColor();
   cout << " ";
 }
 
@@ -47,41 +56,86 @@ void uncover (int x, int y) {
   }
 }
 
+int menu (string title, const char *options[], int n) {
+  int x = 0;
+  while (true) {
+    rlutil::cls();
+    cout << title << endl;
+
+    for (int i = 0; i < n; ++i) {
+      if (x==i) {
+        rlutil::setColor(2);
+        cout << "> ";
+        rlutil::resetColor();
+      }
+      else cout << "  ";
+      cout << options[i] << endl;
+    }
+
+    int key = rlutil::getkey();
+
+    if (key == 1) return x;
+
+    else if (key == 0) exit(0);
+
+    else if (key == 14) {
+      if (x==0) continue;
+      x--;
+    }
+
+    else if (key == 15) {
+      if (x==n) continue;
+      x++;
+    }
+  }
+}
+
 int main () {
   srand (time(NULL));
   rlutil::saveDefaultColor();
+  rlutil::hidecursor();
 
   int bombs, flags;
 
-
   //Menu
-  while (true) {
+  const char *opts[3];
+  opts[0] = "New Game";
+  opts[1] = "Credits";
+  opts[2] = "Exit";
+  int opt = menu("Minesweeper++", opts, 3);
+
+  if (opt == 2) return 0;
+  else if (opt == 1) {
     rlutil::cls();
     cout << "Minesweeper++" << endl;
-    cout << "Enter difficulty (beginner/intermediate/expert): ";
-    string dif;
-    cin >> dif;
+    cout << "Created by Arisien (https://github.com/Arisien)" << endl;
+    cout << "Thanks to tapio for rlutil (https://github.com/tapio/rlutil)" << endl;
+    return 0;
+  }
 
-    if (dif == "b" || dif == "beginner") {
-      height = 9;
-      width = 9;
-      bombs = 10;
-      break;
-    }
+  //Difficulty menu
+  const char *difs[3];
+  difs[0] = "Beginner";
+  difs[1] = "Intermediate";
+  difs[2] = "Expert";
+  int dif = menu("New Game", difs, 3);
 
-    else if(dif == "i" || dif == "intermediate") {
-      height = 16;
-      width = 16;
-      bombs = 40;
-      break;
-    }
+  if (dif==0) { //beginner
+    height = 9;
+    width = 9;
+    bombs = 10;
+  }
 
-    else if(dif == "e" || dif == "expert") {
-      height = 24;
-      width = 24;
-      bombs = 99;
-      break;
-    }
+  else if(dif==1) { //intermediate
+    height = 16;
+    width = 16;
+    bombs = 40;
+  }
+
+  else if(dif==2) { //expert
+    height = 24;
+    width = 24;
+    bombs = 99;
   }
 
   flags = bombs;
@@ -114,6 +168,8 @@ int main () {
   }
 
   //Game loop
+  int x = 0;
+  int y = 0;
   while (true) {
     rlutil::cls();
     bool gameOver = false;
@@ -121,16 +177,9 @@ int main () {
 
     //Field Rendering
     for(int i=0; i<height; i++) {
-      if (i == 0) {
-        cout << "M ";
-        for(int j=0; j<width; j++) printh(j+1);
-        cout << endl;
-      }
-
-      printh(i+1);
-
       for (int j=0; j<width; j++) {
-        printc(i,j);
+        if(i == y && x == j) printc(i,j, true);
+        else printc(i, j, false);
         if(!field[i][j].visible && !field[i][j].flagged) gameWon = false;
         if(field[i][j].visible && field[i][j].value == 9) gameOver = true;
       }
@@ -148,50 +197,58 @@ int main () {
 
     cout << endl << "Flags: " << flags << endl;
 
-    string cmd;
-    cout << endl << "$ ";
-    cin >> cmd;
+    int key = rlutil::getkey();
 
-    if (cmd == "plot") {
-      int x, y;
-      cin >> x;
-      cin >> y;
-      x--;
+    if (key == 0) return 0;
+
+    else if (key == 14) {
+      if (y == 0) continue;
       y--;
+    }
+
+    else if (key == 15) {
+      if (y == height-1) continue;
+      y++;
+    }
+
+    else if (key == 16) {
+      if (x == 0) continue;
+      x--;
+    }
+
+    else if (key == 17) {
+      if (x == width-1) continue;
+      x++;
+    }
+
+    else if (key == 1) {
       if (field[y][x].flagged) continue;
-      //For already uncovered points, all adjacant points are uncovered
-      //To be added: safety feature if N amount of flags havent been placed adjacently yet
-      if (field[y][x].visible){
-        if(!field[y+1][x+1].visible && !field[y+1][x+1].flagged) uncover(x+1, y+1);
-        if(!field[y][x+1].visible && !field[y][x+1].flagged) uncover(x+1, y);
-        if(!field[y-1][x+1].visible && !field[y-1][x+1].flagged) uncover(x+1, y-1);
-        if(!field[y+1][x].visible && !field[y+1][x].flagged) uncover(x, y+1);
-        if(!field[y-1][x].visible && !field[y-1][x].flagged) uncover(x, y-1);
-        if(!field[y+1][x-1].visible && !field[y+1][x-1].flagged) uncover(x-1, y+1);
-        if(!field[y][x-1].visible && !field[y][x-1].flagged) uncover(x-1, y);
-        if(!field[y-1][x-1].visible && !field[y-1][x-1].flagged) uncover(x-1, y-1);
-      }
+      // //For already uncovered points, all adjacant points are uncovered
+      // //To be added: safety feature if N amount of flags havent been placed adjacently yet
+      // if (field[y][x].visible){
+      //   if(!field[y+1][x+1].visible && !field[y+1][x+1].flagged) uncover(x+1, y+1);
+      //   if(!field[y][x+1].visible && !field[y][x+1].flagged) uncover(x+1, y);
+      //   if(!field[y-1][x+1].visible && !field[y-1][x+1].flagged) uncover(x+1, y-1);
+      //   if(!field[y+1][x].visible && !field[y+1][x].flagged) uncover(x, y+1);
+      //   if(!field[y-1][x].visible && !field[y-1][x].flagged) uncover(x, y-1);
+      //   if(!field[y+1][x-1].visible && !field[y+1][x-1].flagged) uncover(x-1, y+1);
+      //   if(!field[y][x-1].visible && !field[y][x-1].flagged) uncover(x-1, y);
+      //   if(!field[y-1][x-1].visible && !field[y-1][x-1].flagged) uncover(x-1, y-1);
+      // }
       else uncover(x, y);
     }
 
-    else if (cmd == "flag") {
-      int x, y;
-      cin >> x;
-      cin >> y;
-      if(field[y-1][x-1].visible) continue;
-      if(!field[y-1][x-1].flagged) {
+    else if (key == 102 || key == 70) {
+      if(field[y][x].visible) continue;
+      if(!field[y][x].flagged) {
         if(flags == 0) continue;
-        field[y-1][x-1].flagged = true;
+        field[y][x].flagged = true;
         flags--;
       }
       else {
-        field[y-1][x-1].flagged = false;
+        field[y][x].flagged = false;
         flags++;
       }
-    }
-
-    else if (cmd == "quit") {
-      return 0;
     }
 
   }
